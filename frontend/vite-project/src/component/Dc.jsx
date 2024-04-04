7ebcaeac4d3e16125964e688b60e6d20b832cb70
Your Name <your_email@example.com>
2024-04-04 07:29:29 +0530

    commit

--- frontend/vite-project/src/component/Dc.jsx
+++ frontend/vite-project/src/component/Dc.jsx
@@ -60,37 +60,17 @@ function Dc() {
                   src={datas1.Image}
                   alt=""
                 />
-                <div className="p-4">
-                  <p className="font-bold text-xl">{datas1.Hero_Name}</p>
                
-                  <p className="text-gray-600">{datas1.Real_Name}</p>
-                  <p className="mt-2">
-                    <span className="font-bold">Superpower:</span>{" "}
-                    {datas1.Superpower}
-                  </p>
-                  <div style={{ maxHeight: "300px", overflowY: "auto" }}>
-                    <p className="mt-2">
-                      <span className="font-bold">First Appearance:</span>{" "}
-                      {datas1.First_Appearance}
-                    </p>
-                    <p className="mt-2">
-                      <span className="font-bold">Costume Quirk:</span>{" "}
-                      {datas1.Costume_Quirk}
-                    </p>
-                    <p className="mt-2">
-                      <span className="font-bold">Catchphrase:</span>{" "}
-                      {datas1.Catchphrase}
-                    </p>
-                    <p className="mt-2">
-                      <span className="font-bold">Backstory:</span>{" "}
-                      {datas1.Backstory}
-                    </p>
-                    <p className="mt-2">
-                      <span className="font-bold">Most Useless Moment:</span>{" "}
-                      {datas1.Most_Useless_Moment}
-                    </p>
-                  </div>
-                </div>
+               <div className='p-4'>
+              <p className='font-bold text-xl'>{datas1.Hero_Name}</p>
+              <p className='text-gray-600'>{datas1.Real_Name}</p>
+              <p className='mt-2'><span className='font-bold'>Superpower:</span> {datas1.Superpower}</p>
+              <p className='mt-2'><span className='font-bold'>First Appearance:</span> {datas1.First_Appearance}</p>
+              <p className='mt-2'><span className='font-bold'>Costume Quirk:</span> {datas1.Costume_Quirk}</p>
+              <p className='mt-2'><span className='font-bold'>Catchphrase:</span> {datas1.Catchphrase}</p>
+              <p className='mt-2'><span className='font-bold'>Backstory:</span> {datas1.Backstory}</p>
+              <p className='mt-2'><span className='font-bold'>Most Useless Moment:</span> {datas1.Most_Useless_Moment}</p>
+            </div>
               </div>
             ))}
           {data1 &&
@@ -106,36 +86,19 @@ function Dc() {
                   src={datas1.Image}
                   alt=""
                 />
-                <div className="p-4">
-                  <p className="font-bold text-xl">{datas1.Hero_Name}</p>
-                  <p className="text-gray-600">{datas1.Real_Name}</p>
-                  <p className="mt-2">
-                    <span className="font-bold">Superpower:</span>{" "}
-                    {datas1.Superpower}
-                  </p>
-                  <div style={{ maxHeight: "300px", overflowY: "auto" }}>
-                    <p className="mt-2">
-                      <span className="font-bold">First Appearance:</span>{" "}
-                      {datas1.First_Appearance}
-                    </p>
-                    <p className="mt-2">
-                      <span className="font-bold">Costume Quirk:</span>{" "}
-                      {datas1.Costume_Quirk}
-                    </p>
-                    <p className="mt-2">
-                      <span className="font-bold">Catchphrase:</span>{" "}
-                      {datas1.Catchphrase}
-                    </p>
-                    <p className="mt-2">
-                      <span className="font-bold">Backstory:</span>{" "}
-                      {datas1.Backstory}
-                    </p>
-                    <p className="mt-2">
-                      <span className="font-bold">Most Useless Moment:</span>{" "}
-                      {datas1.Most_Useless_Moment}
-                    </p>
-                  </div>
-                </div>
+                <div className='p-4'>
+              <p className='font-bold text-xl'>{datas1.Hero_Name}</p>
+              <p className='text-gray-600'>{datas1.Real_Name}</p>
+
+              <div style={{ maxHeight: '300px', overflowY: 'auto' }}>
+                <p className='mt-2'><span className='font-bold'>Superpower:</span> {datas1.Superpower}</p>
+                <p className='mt-2'><span className='font-bold'>First Appearance:</span> {datas1.First_Appearance}</p>
+                <p className='mt-2'><span className='font-bold'>Costume Quirk:</span> {datas1.Costume_Quirk}</p>
+                <p className='mt-2'><span className='font-bold'>Catchphrase:</span> {datas1.Catchphrase}</p>
+                <p className='mt-2'><span className='font-bold'>Backstory:</span> {datas1.Backstory}</p>
+                <p className='mt-2'><span className='font-bold'>Most Useless Moment:</span> {datas1.Most_Useless_Moment}</p>
+              </div>
+            </div>
               </div>
             ))}
           <div className="rounded-lg shadow-2xl bg-white m-4 p-40 h-96">
